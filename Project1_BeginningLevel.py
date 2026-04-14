8fa9409dd2172cf74b635e7a64274268cfba7639
dhanushasus06-svg <dhanushasus06@gmail.com>
2026-04-14 08:29:17 +0530

    Add files via upload

---
+++ Project1_BeginningLevel.py
@@ -0,0 +1,23 @@
+#Basic_Python_Calculator 
+
+Number_1 = float(input("Enter the first number : "))        
+Number_2 = float(input("Enter the second number : "))
+Operator = input("Enter any of the listed operator(+,_,*,/) : ")
+
+if Operator == "+" :
+    print("Output(Addition) : ", Number_1+Number_2)
+elif Operator == "-" :
+    print("Output(Substraction) :", Number_1-Number_2)
+elif Operator == "*" :
+    print("Output(Multiplication) :", Number_1*Number_2)
+elif Operator == "/" :
+    if(Number_2!=0):
+        print("Output(Divsion) :", Number_1/Number_2)
+    else:
+        print(f"{Number_1}/{Number_2} is not divisible")
+else:
+    print("Invalid Operator")imno
+
+    
+
+
